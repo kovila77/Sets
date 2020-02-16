cda2b7ba5fd746efdd174fcae4609602fdd66c01
kovila77 <354981@mail.ru>
2020-02-16 23:03:08 +0500

    add communication with people

--- Program.cs
+++ Program.cs
@@ -13,9 +13,9 @@ namespace Sets
         {
             //Tests.MultiSetTest1();
 
-            string dataFileName = @"data.txt";
-            string setFileName = @"set.txt";
-            int lastChoice;
+            int typeOfSet;
+            int typeOfInput;
+            string FileWithElementsName;
             int max;
             Set set;
 
@@ -27,68 +27,122 @@ namespace Sets
 
             try
             {
-                lastChoice = Convert.ToInt32(Console.ReadLine());
+                typeOfSet = Convert.ToInt32(Console.ReadLine());
             }
             catch (Exception)
             {
-                //Загружаем прошлый выбор
-                try
-                {
-                    using (StreamReader dataReader = new StreamReader(dataFileName))
-                    {
-                        lastChoice = Convert.ToInt32(dataReader.ReadLine());
-                    }
-                }
-                catch (Exception)
-                {
-                    lastChoice = 1;
-                }
+                typeOfSet = Settings.Default.TypeSet;
             }
             Console.Write("Выбрано ");
-            switch (lastChoice)
+            switch (typeOfSet)
             {
-                case 1:
-                    {
-                        Console.WriteLine("Логический");
-                        break;
-                    }
-                case 2:
-                    {
-                        Console.WriteLine("Битовый");
-                        break;
-                    }
-                case 3:
-                    {
-                        Console.WriteLine("Мультимножество");
-                        break;
-                    }
+                case 1: { Console.WriteLine("Логический"); break; }
+                case 2: { Console.WriteLine("Битовый"); break; }
+                case 3: { Console.WriteLine("Мультимножество"); break; }
                 default:
-                    {
-                        lastChoice = 1;
-                        Console.WriteLine("Логический");
-                        break;
-                    }
+                    { typeOfSet = 1; Console.WriteLine("Логический"); break; }
             }
-            //Сохраняем текущий выбор
-            try
-            {
-                using (StreamWriter dataWriter = new StreamWriter(dataFileName))
-                {
-                    dataWriter.WriteLine(lastChoice.ToString());
-                }
-            }
-            catch (Exception) { }
+            Settings.Default.TypeSet = typeOfSet;
+            Settings.Default.Save();
 
             Console.WriteLine("Введите максимум во множестве:");
             try
             {
                 max = Convert.ToInt32(Console.ReadLine());
+                if (max < 0) throw new Exception();
             }
             catch (Exception)
             {
-                max = 1;
+                max = Settings.Default.MaxElem;
+            }
+            Settings.Default.MaxElem = max;
+            Console.WriteLine($"Максимум = {max}");
+
+            switch (typeOfSet)
+            {
+                case 1: { set = new SimpleSet(max); break; }
+                case 2: { set = new BitSet(max); break; }
+                case 3: { set = new MultiSet(max); break; }
+                default:
+                    { set = new SimpleSet(max); break; }
             }
 
+            Console.WriteLine("Ввод множества:");
+            Console.WriteLine("1 Строка");
+            Console.WriteLine("2 Файл");
+            try
+            {
+                typeOfInput = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                typeOfInput = Settings.Default.TypeSet;
+            }
+            switch (typeOfInput)
+            {
+                case 2:
+                    {
+                        Console.WriteLine($"Использовать файл {Settings.Default.FileWithElements}? (\"н\" для выбора другого файла)");
+                        string choice = Console.ReadLine();
+                        if (choice != "н") { FileWithElementsName = Settings.Default.FileWithElements; }
+                        else
+                        {
+                            Console.WriteLine("Введите имя файла:");
+                            choice = Console.ReadLine();
+                            if (File.Exists(choice))
+                            {
+                                FileWithElementsName = choice;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Такого файла не существует");
+                                FileWithElementsName = Settings.Default.FileWithElements;
+                            }
+                        }
+                        Console.WriteLine($"Используется файл {FileWithElementsName}"); ;
+                        //string str;
+                        List<int> tmp = new List<int>();
+                        try
+                        {
+                            using (StreamReader sr = new StreamReader(FileWithElementsName))
+                            {
+                                while (!sr.EndOfStream)
+                                {
+                                    try
+                                    {
+                                        tmp.Add(Convert.ToInt32(sr.ReadLine()));
+                                    }
+                                    catch (Exception)
+                                    {
+                                        continue;
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                            throw;
+                        }
+                        set.FillSet(tmp.ToArray());
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Введите строку элементов множества,\nразделённых символом пробел (пример \"1 2 4 5\"):");
+                        set.FillSet(Console.ReadLine());
+                        break;
+                    }
+            }
+
+            string command = Console.ReadLine();
+            while (command != "exit")
+            {
+                set.Print(Console.WriteLine);
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Для выхода нажмите любую кнопку...");
             Console.ReadKey();
         }
     }
